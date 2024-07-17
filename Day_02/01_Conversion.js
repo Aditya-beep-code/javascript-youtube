c7a6658db27ecd19c7e1dfb26644e318a3005934
Aditya-beep-code <ason80108@gmail.com>
2024-07-17 05:29:18 +0000

    part2 conversion

--- Day_02/01_Conversion.js
+++ Day_02/01_Conversion.js
@@ -30,9 +30,46 @@ console.log(IsBoolean)
 //"" = false
 // "Aditya" = true*/
 
-let someNumber = 33
+/*let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(someNumber)
 console.log(typeof someNumber)
 console.log(stringNumber)
-console.log(typeof stringNumber)
+console.log(typeof stringNumber)*/
+
+//******operations******//
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+/*console.log(2 + 2)
+console.log(2 - 2)
+console.log(2 / 2)
+console.log(2 * 2)*/
+
+
+let str1 = "Hello"
+let str2 = " Aditya"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(+true)
+console.log(+"")
+
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+console.log(gameCounter)
+gameCounter++;
+console.log(gameCounter)
+
+
