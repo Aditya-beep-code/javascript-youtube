07d629baabbe3b1cb09ff19b3ba5b15356dba86e
Aditya-beep-code <ason80108@gmail.com>
2024-07-17 04:53:45 +0000

    Conversion Number

---
+++ Day_02/01_Conversion.js
@@ -0,0 +1,38 @@
+/*let score = "33abc"
+//var age = 22
+
+console.log(typeof score)
+console.log(typeof(score));//as a method
+//console.log(typeof age);
+//type Conversion
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+//console.log(typeof score)
+console.log(valueInNumber)*/
+/*let score = true
+console.log(score)
+console.log(typeof null)
+let InNumber = Number(score)
+console.log(InNumber)*/
+
+//"33"=33
+//"33abc" = NaN
+//true = 1
+//false = 0
+//convert the value in a boolean form
+/*let isLoggedIn = ""
+console.log(isLoggedIn)
+console.log(typeof isLoggedIn)
+let IsBoolean = Boolean(isLoggedIn)
+console.log(IsBoolean)
+//1 = true
+//0 = false
+//"" = false
+// "Aditya" = true*/
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(someNumber)
+console.log(typeof someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
