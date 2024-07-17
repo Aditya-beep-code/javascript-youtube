b718923360afa933452285fc21d714fe95575d17
Aditya-beep-code <ason80108@gmail.com>
2024-07-17 08:15:06 +0000

    Datatype Summary

---
+++ Day_02/Summary.js
@@ -0,0 +1,37 @@
+//primitve datatype
+
+//7 categories
+//String,Number,Boolean,null,undefined,Symbol,BigInt
+//javascript is a dynamically typed language
+/*const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false;
+const outsideTemp = null
+let userEmail;
+//declaration of symbol
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id==anotherId);
+
+const bigNumber = 34521282171872737367n*/
+
+
+
+
+//Reference or non-primitive data type
+//array
+const heros = ["shakitman","naagraj","doga"]
+//objects
+ let myObj = {
+    name : "Aditya",
+    age : 22,
+}
+
+
+//function
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myFunction )
+//arrays,Objects,Functions
